79d3edd281163e1bd09ade09c86fad4d821d79f2
samuel <sammykuria300@gmail.com>
2023-09-21 13:22:30 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1= 2
+let num2 = 31
+let multiply = num1 * num2
+console.log(multiply)
+
+
+const random= Math.floor(Math.random() * 100) + 1;
+console.log(random)
+
+
+
+let num3=14
+let num4=10
+let mod = num3%num4
+
+const numbers = [10, 5, 20, 15, 8];
+const max = Math.max(...numbers); // Using the spread operator to pass the array elements as arguments
+
+console.log(max); // This will print 20
